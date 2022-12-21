0856faa98671cdd05005d0cf3caaac42fcf5109a
Maksim <mgorbiik@gmail.com>
2022-12-21 22:55:10 +0200

    Create get_data.sql

---
+++ laboratory-work-8/get_data.sql
@@ -0,0 +1,10 @@
+create or replace function get_data(university_name varchar)
+    returns table (u_id integer, name varchar, year integer) as $$
+declare
+    query_str varchar;
+begin
+    query_str := 'select u_id, name, year from hotel where name = ''' || university_name || '''';
+    raise notice 'Query=%', query_str;
+    return query execute query_str;
+end;
+$$ language plpgsql;

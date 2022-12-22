8ea07fc5c6d2bed655ca3551af31e5bfe6a1132a
Maksim <mgorbiik@gmail.com>
2022-12-22 18:39:58 +0200

    Create change-data.sql

---
+++ laboratory-work-8/change-data.sql
@@ -0,0 +1,14 @@
+drop function if exists change_data(character varying, character varying)
+create function change_data(old_name varchar, new_name varchar) returns integer as
+$$
+declare
+    query_str    varchar;
+    count_result integer;
+begin
+    select count(*) into count_result from university where name = old_name;
+    query_str := 'update university set name = ''' || new_name || ''' where name = ''' || old_name || '''';
+    raise notice 'Query: %', query_str;
+    execute query_str;
+    return count_result;
+end ;
+$$ language plpgsql;
